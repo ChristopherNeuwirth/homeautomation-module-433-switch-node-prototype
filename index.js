e09b72ac0f8fb02a6f16f984da7ec4df026fa004
Christopher <cn@wtf-solutions.de>
2016-10-22 13:19:05 +0200

    Further Testing

--- index.js
+++ index.js
@@ -6,23 +6,35 @@
 
 let pkg = require('./package.json');
 let rpi433    = require('rpi-433'),
-    rfSniffer = rpi433.sniffer({
-      pin: 2,                     //Snif on GPIO 2 (or Physical PIN 13)
-      debounceDelay: 500          //Wait 500ms before reading another code
-    }),
+    // rfSniffer = rpi433.sniffer({
+    //   pin: 2,                     //Snif on GPIO 2 (or Physical PIN 13)
+    //   debounceDelay: 500          //Wait 500ms before reading another code
+    // }),
     rfEmitter = rpi433.emitter({
       pin: 0,                     //Send through GPIO 0 (or Physical PIN 11)
-      pulseLength: 180            //Send the code with a 350 pulse length
+      pulseLength: 180            //Send the code with a 180 pulse length
     });
 
 //rpi-433 uses the kriskowal's implementation of Promises so,
 //if you prefer Promises, you can also use this syntax :
-rfEmitter.sendCode(5575987, {pin: 0, pulseLength: 179})
-  .then(function(stdout) {
-    console.log('Code sent: ', stdout);
-  }, function(error) {
-    console.log('Code was not sent, reason: ', error);
-  });
+// child_process not killed propperly
+// rfEmitter.sendCode(5575987, {pin: 0, pulseLength: 179})
+//   .then(function(stdout) {
+//     console.log('Code sent: ', stdout);
+//   }, function(error) {
+//     console.log('Code was not sent, reason: ', error);
+//   });
 
+
+// Code On: 5575987
+// Code Off: 5575996
+
+rfEmitter.sendCode(5575996, function(error, stdout) {
+  if(error) {
+    console.log('Code was not sent, reason: ', error);
+  } else {
+    console.log('Code sent: ', stdout);
+  };
+});
 
 console.log('switch version ' + pkg.version);
